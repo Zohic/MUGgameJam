81a5c4c74782dc789d8fb20f63b1c13ede82d7c3
Zohic <albertorandyr@gmail.com>
2021-06-19 17:31:01 +0300

    Added Simple Camera Scroll with mouse

---
+++ MUGGameJam/Assets/ScrollControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollControl : MonoBehaviour
+{
+    [SerializeField]
+    Camera scrollCamera;
+    public float scrollSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.mouseScrollDelta.y > 0)
+        {
+            scrollCamera.transform.Translate(scrollSpeed * Vector3.right);
+        }else if(Input.mouseScrollDelta.y < 0)
+        {
+            scrollCamera.transform.Translate(scrollSpeed * Vector3.left);
+        }
+    }
+}
