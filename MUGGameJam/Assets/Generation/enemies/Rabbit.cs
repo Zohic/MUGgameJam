1d5d0385d49337f5cbc3ff5dd00aebc7701f113f
Zohic <albertorandyr@gmail.com>
2021-06-20 19:17:43 +0300

    testAgain

--- MUGGameJam/Assets/Generation/enemies/Rabbit.cs
+++ MUGGameJam/Assets/Generation/enemies/Rabbit.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Rabbit : WalkingEnemy
 {
-    // Start is called before the first frame update
+    public GameObject laserPrefab;
+    public Transform laserOut;
+    public float laserSpeed;
     void Start()
     {
         base.Start();
     }
 
-    // Update is called once per frame
+    void ShootLaser(Vector3 dir)
+    {
+        Quaternion rot = Quaternion.LookRotation(dir, Vector3.forward);
+
+        
+        ProjectTile las = Instantiate(laserPrefab, laserOut.position, Quaternion.Euler(0, 0, rot.eulerAngles.z)).GetComponent<ProjectTile>();
+        las.speed = dir * laserSpeed;
+        Debug.Log("shooting");
+    }
+
     void Update()
     {
         base.Update();
+        if(Input.GetKeyDown(KeyCode.W))
+            ShootLaser(new Vector3(1, 0, 0));
     }
 }
